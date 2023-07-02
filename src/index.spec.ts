d94cdd886cd2e3e8fa2ce9c84170f10f4447c39d
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-02 20:38:47 +0530

    Replace jest with vitest

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,4 @@
-import { expect, it } from '@jest/globals';
+import { expect, it } from 'vitest';
 import { renderHook, act } from '@testing-library/react';
 
 import useCheckboxTree from '.';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: 'happy-dom',
+  },
 });
