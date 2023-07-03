8e8a253c9e9c3407a172b9ab80a5be62b357d0bb
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-07-03 12:30:27 +0530

    Update storybook theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'UseCheckboxTree',
+  brandUrl: 'https://github.com/sibiraj-s/use-checkbox-tree',
+  brandTarget: '_blank',
+});
