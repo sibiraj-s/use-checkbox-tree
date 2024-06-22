ecba16e66fc0f34d53f228fd043a7ea4d231b0e7
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-06-22 13:57:08 +0530

    Bump devDependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,29 +1,25 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    // {
-    //   name: '@storybook/addon-styling',
-    //   options: {
-    //     // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-    //     // For more details on this addon's options.
-    //     postCss: true,
-    //   },
-    // },
-  ],
+
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
   docs: {
     autodocs: 'tag',
   },
+
   core: {
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 };
 export default config;
